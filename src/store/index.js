54f67b6b89aff34db030df751aab6ee4540c0bac
zhongyingdong <1069441413@qq.com>
2022-09-24 17:00:29 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,69 @@
 import { createStore } from 'vuex'
-
-export default createStore({
+// 模块的使用
+const card = {
+  // 不加name
   state: {
+    name: 'moduleA'
+  },
+  getters: {
+    newName (state) {
+      return state.name + 666
+    }
+  }
+}
+
+const goods = {
+  // 命名模块
+  namespaced: true,
+  state: {
+    name: 'moduleB'
+  },
+  getters: {
+    newName (state) {
+      return state.name + 999
+    }
   },
   mutations: {
+    updateName (state, payload) {
+      state.name = payload
+    }
   },
   actions: {
-  },
+    updateName ({ commit }, payload) {
+      setTimeout(() => {
+        commit('updateName', payload)
+      }, 1000)
+    }
+  }
+}
+
+export default createStore({
   modules: {
+    card,
+    goods
   }
 })
+
+// export default createStore({
+//   state: {
+//     name: '张三'
+//   },
+//   getters: {
+//     newName (state) {
+//       return state.name + '666'
+//     }
+//   },
+//   mutations: {
+//     updateName (state, payload) {
+//       state.name = payload
+//     }
+//   },
+//   actions: {
+//     updateName ({ commit }, payload) {
+//       setTimeout(() => {
+//         commit('updateName', payload)
+//       }, 1000)
+//     }
+//   },
+//   modules: {}
+// })
