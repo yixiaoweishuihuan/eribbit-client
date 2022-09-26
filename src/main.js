b98e7e71634541072e5741b63540c979a3f448a3
zhongyingdong <1069441413@qq.com>
2022-09-26 19:08:16 +0800

    初始化样式和全局样式 混入式样式等

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store/index'
 
+// 初始化样式
+import 'normalize.css'
+// 重置样式
+import './assets/styles/common.less'
+
 createApp(App).use(store).use(router).mount('#app')
